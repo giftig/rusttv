a63462050ff87e30ce46037aca23201e704262d2
Rob Moore <giftiger.wunsch@gmail.com>
2024-01-05 21:39:20 +0000

    Episode parsing
    TV show matching, season/episode matching. Initial implementation, MVP
    version for now.

--- src/episode.rs
+++ src/episode.rs
@@ -1,7 +1,82 @@
+#[cfg(test)]
+mod tests;
+
+use std::fs::canonicalize;
+use std::path::Path;
+
+use regex::Regex;
+
+#[derive(Debug, PartialEq)]
 pub struct Episode {
-    pub local_path: Path,
+    pub local_path: String,
     pub show_name: String,
     pub season_num: u32,
     pub episode_num: u32,
     pub ext: String
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ParseError {
+    BadShow,
+    BadFilename,
+    BadPath,
+    BadExtension
+}
+
+impl Episode {
+    pub fn derive_show(show: &str, known_shows: &Vec<String>) -> Option<String> {
+        let s = show.to_string();
+
+        if known_shows.contains(&s) {
+            return Some(s);
+        }
+
+        // TODO: Use strsum to find the closest match and compare to some static certainty
+        // threshold and return that if appropriate
+        None
+    }
+
+    // Parse filename into season, episode, and extension; e.g. S01 E01.mkv -> (1, 1, mkv)
+    pub fn parse_filename(filename: &str) -> Option<(u32, u32, String)> {
+        // TODO: Use the smart-rename regexes to match the various supported patterns
+        let pattern = Regex::new(r"^S([0-9]{2}) E([0-9]{2})\.([^\.]+)$").unwrap();
+
+        let caps = pattern.captures(filename)?;
+        let season = caps.get(1)?.as_str().parse::<u32>().unwrap();
+        let episode = caps.get(2)?.as_str().parse::<u32>().unwrap();
+        let ext = caps.get(3)?.as_str().to_string();
+
+        Some((season, episode, ext))
+    }
+
+    pub fn from(path: &str, known_shows: &Vec<String>, allowed_exts: &Vec<String>) -> Result<Episode, ParseError> {
+        let abs_path = canonicalize(Path::new(&path)).map_err(|_| ParseError::BadPath)?;
+        let comps: Vec<&str> = abs_path.iter().map(|s| { s.to_str().unwrap() }).collect();
+
+        if comps.len() <= 1 {
+            return Err(ParseError::BadPath);
+        }
+
+        let raw_show = comps[comps.len() - 2];
+        let filename = comps[comps.len() - 1];
+
+        let show_name = Self::derive_show(raw_show, known_shows).ok_or(ParseError::BadShow)?;
+        let (season_num, episode_num, ext) = Self::parse_filename(&filename).ok_or(ParseError::BadFilename)?;
+
+        if !allowed_exts.contains(&ext) {
+            return Err(ParseError::BadExtension);
+        }
+
+        Ok(Episode {
+            local_path: path.to_string(),
+            show_name: show_name,
+            season_num: season_num,
+            episode_num: episode_num,
+            ext: ext
+        })
+    }
+
+    pub fn remote_subpath(&self) -> String {
+        format!("{}/S{:02} E{:02}.{}", self.show_name, self.season_num, self.episode_num, self.ext)
+    }
 }
---
+++ src/episode/tests.rs
@@ -0,0 +1,63 @@
+use super::*;
+
+use std::fs;
+use std::fs::OpenOptions;
+use std::io;
+use std::path::Path;
+
+// Touch the given test file, creating a dir path to it as we go
+fn touch_file(path: &Path) -> io::Result<()> {
+    fs::create_dir_all(path.parent().unwrap()).ok();
+    OpenOptions::new().create(true).write(true).open(path).map(|_| { () })
+}
+
+fn tv_shows() -> Vec<String> {
+    vec![
+        "All My Circuits",
+        "Everybody Loves Hypnotoad",
+        "Calculon: A Calculon Story"
+    ].into_iter().map(String::from).collect()
+}
+
+fn allowed_exts() -> Vec<String> {
+    vec!["mkv", "mp4"].into_iter().map(String::from).collect()
+}
+
+#[test]
+fn valid_episode() {
+    let path = "/tmp/rusttv-tests/All My Circuits/S01 E02.mkv";
+    touch_file(Path::new(path)).unwrap();
+
+    let expected = Episode {
+        local_path: String::from(path),
+        show_name: String::from("All My Circuits"),
+        season_num: 1,
+        episode_num: 2,
+        ext: String::from("mkv")
+    };
+
+    let actual = Episode::from(path, &tv_shows(), &allowed_exts()).unwrap();
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn bad_path() {
+    let path = "/";
+
+    let expected = ParseError::BadPath;
+    let actual = Episode::from(path, &tv_shows(), &allowed_exts()).unwrap_err();
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn bad_show_name() {
+    let path = "/tmp/rusttv-tests/Totally Unknown Show/S01 E01.mkv";
+    touch_file(Path::new(path)).unwrap();
+
+    let expected = ParseError::BadShow;
+    let actual = Episode::from(path, &tv_shows(), &allowed_exts()).unwrap_err();
+
+    assert_eq!(actual, expected);
+}
